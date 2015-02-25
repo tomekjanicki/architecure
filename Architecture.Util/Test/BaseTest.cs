4d02d18680a992892765df6b7ee5dd59d59f809e
tjak <tomasz.janicki@impaqgroup.com>
2015-02-25 21:47:11 +0100

    log4net

--- Architecture.Util/Test/BaseTest.cs
+++ Architecture.Util/Test/BaseTest.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using log4net;
 using NUnit.Framework;
+using Architecture.Util.Log4Net;
 
 namespace Architecture.Util.Test
 {
     public class BaseTest
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(BaseTest));
+
         [TestFixtureSetUp]
         public virtual void TestFixtureSetUp()
         {
+            
         }
 
         [TestFixtureTearDown]
@@ -22,18 +24,14 @@ namespace Architecture.Util.Test
         [SetUp]
         public virtual void SetUp()
         {
-            Message(string.Format("Setting up test: {0}", TestContext.CurrentContext.Test.FullName));            
+            log4net.Config.XmlConfigurator.Configure();
+            Logger.Info(() => string.Format("Setting up test {0}", TestContext.CurrentContext.Test.FullName));
         }
 
         [TearDown]
         public virtual void TearDown()
         {
-            Message(string.Format("Tearing down up test: {0}", TestContext.CurrentContext.Test.FullName));            
-        }
-
-        protected void Message(string message)
-        {
-            Trace.WriteLine(string.Format("Time: {0}: Thread Id: {1}, Msg: {2}", DateTime.Now.ToString("hh:mm:ss.ffff"), Thread.CurrentThread.ManagedThreadId, message));
+            Logger.Info(() => string.Format("Tearing down up test {0}", TestContext.CurrentContext.Test.FullName));
         }
 
         protected static async Task<T> Delayed<T>(int miliseconds, T result)
